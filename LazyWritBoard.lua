1e4e0f26ac4a6c2676fb87016dc5098fbed4ec1c
silvereyes <silvereyes333@gmail.com>
2017-01-19 13:52:26 -0600

    Version 1.1.0
    - Automatically complete writ quests

--- LazyWritBoard.lua
+++ LazyWritBoard.lua
@@ -1,6 +1,12 @@
 local addonName = "LazyWritBoard"
+local function HandleQuestCompleteDialog(eventCode, journalIndex)
+    -- Stop listening for quest complete dialog
+    EVENT_MANAGER:UnregisterForEvent(addonName, EVENT_QUEST_COMPLETE_DIALOG)
+    -- Complete the writ quest
+    CompleteQuest()
+end
 local function HandleEventQuestOffered(eventCode)
-    -- Stop listen for quest offering
+    -- Stop listening for quest offering
     EVENT_MANAGER:UnregisterForEvent(addonName, EVENT_QUEST_OFFERED)
     -- Accept the writ quest
     AcceptOfferedQuest()
@@ -18,10 +24,30 @@ local function HandleChatterBegin(eventCode, optionCount)
         EVENT_MANAGER:RegisterForEvent(addonName, EVENT_QUEST_OFFERED, HandleEventQuestOffered)
         -- Select the first writ
         SelectChatterOption(1)
+        
+    -- If it is a writ quest completion option
+    elseif optionType == CHATTER_START_ADVANCE_COMPLETABLE_QUEST_CONDITIONS
+       and string.find(optionString, "Place the goods") ~= nil  
+    then
+        -- Listen for the quest complete dialog
+        EVENT_MANAGER:RegisterForEvent(addonName, EVENT_QUEST_COMPLETE_DIALOG, HandleQuestCompleteDialog)
+        -- Select the first option to complete the quest
+        SelectChatterOption(1)
+    
+    -- If the goods were already placed, then complete the quest
+    elseif optionType == CHATTER_START_COMPLETE_QUEST
+       and (string.find(optionString, "Place the goods") ~= nil 
+            or string.find(optionString, "Sign the Manifest") ~= nil)
+    then
+        -- Listen for the quest complete dialog
+        EVENT_MANAGER:RegisterForEvent(addonName, EVENT_QUEST_COMPLETE_DIALOG, HandleQuestCompleteDialog)
+        -- Select the first option to place goods and/or sign the manifest
+        SelectChatterOption(1)
     end
 end
 local function OnAddonLoaded(event, name)
     if name ~= addonName then return end
     EVENT_MANAGER:RegisterForEvent(addonName, EVENT_CHATTER_BEGIN, HandleChatterBegin)
+
 end
 EVENT_MANAGER:RegisterForEvent(addonName, EVENT_ADD_ON_LOADED, OnAddonLoaded)
